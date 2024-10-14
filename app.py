a55c83be60743d6d8cd2a74f05bca7c8c16b1eb3
oritzadok <45933272+oritzadok@users.noreply.github.com>
2024-10-14 03:01:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ def handle():
   print('Request received')
   return "bla"
 
-@app.route('/hello', methods=['POST'])
-def hello():
-  print('Request received')
-  name = request.form.get('name')
-  return "bla"
+# @app.route('/hello', methods=['POST'])
+# def hello():
+#   print('Request received')
+#   name = request.form.get('name')
+#   return "bla"
 
 
 if __name__ == '__main__':
